bd43572f723568b0b7d0a32051f754d8dea9e394
diki_ <diki_@ILIEVPC>
2021-01-17 12:28:23 +0100

    Finish simple Ocelot API Gateway example.

--- API Gateway/Startup.cs
+++ API Gateway/Startup.cs
@@ -19,6 +19,7 @@ namespace API_Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //In memory caching with CacheManager
             services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
         }
 
@@ -31,14 +32,6 @@ namespace API_Gateway
             }
 
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
 
             //set up authentication in the API Gateway
 
